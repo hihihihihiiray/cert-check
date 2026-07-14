cb9bb77776d47bb088e8c0ecce1d3305c996e9c4
raymondnoodles <raymondvu879@gmail.com>
2026-07-13 19:48:39 -0500

    Update SHA1 display to break words and lowercase

--- static/js/index.js
+++ static/js/index.js
@@ -167,7 +167,7 @@ function uploads(p12, password, mp, plist) {
 
                 if (res.sha1) {
                     $('#sha1Row').removeClass('hide');
-                    $('#certSha1').html(res.sha1);
+                    $('#certSha1').css('word-break', 'break-all').html(res.sha1.toLowerCase());
                 }
                 if (res.expirationDate) {
                     lastProvisionMatched = true;
