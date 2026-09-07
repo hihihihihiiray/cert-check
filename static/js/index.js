1e03f5c06524e00e846a6e4c14be73abfdcd40b0
raymondnoodles <raymondvu879@gmail.com>
2026-09-07 01:31:27 -0500

    Reduce fade out and fade in durations for UI elements

--- static/js/index.js
+++ static/js/index.js
@@ -213,7 +213,7 @@ function uploads(p12, password, mp, plist) {
  * ---------------------------------------------------------------------- */
 function toggleRes() {
     // Fade out the results
-    $('#certResult').fadeOut(225, function() {
+    $('#certResult').fadeOut(1, function() {
         // Reset everything after fade completes
         uploading  = false;
         file_item  = null;
@@ -255,7 +255,7 @@ function toggleRes() {
         if (typeof lastProvisionMatched !== 'undefined') lastProvisionMatched = false;
 
         // Fade the upload box back in
-        $('#shortUrl').hide().removeClass('hide').fadeIn(300);
+        $('#shortUrl').hide().removeClass('hide').fadeIn(100);
     });
 }
 
